c35eda429254f94371ebaca7bd724ade9fd0aa33
deujamadhu19 <dehujamadhu@gmail.com>
2026-06-09 09:03:22 +0545

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,22 @@
+import "./App.css"
 
+function App(){
+return(
+  <div>
+    <nav>
+      <div style={{display: 'flex', alignItems:"baseline"}}>
+        <span className="brand-name">
+          fla<em>vour</em>
+        </span>
+        <span className="brand-tagline">React Edition</span>
+      </div>
+      <div className="saved-pill">
+        <span>Saved</span>
+        <span className="saved-pill-count">0</span>
+      </div>
+    </nav>
+  </div>
+);
+}
+
+export default App;
