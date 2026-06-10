eb4952c2f56ea8308891d811942e636433c6bb9b
deujamadhu19 <dehujamadhu@gmail.com>
2026-06-10 09:42:33 +0545

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,17 @@
+import {useState, useEffect} from "react";
 import "./App.css";
 
-function RecipeCard({name, category, image}) {
+function RecipeCard({recipe}) {
   return (
     <div className = "recipe-card">
-      <img className="card-photo" src={image} alt={name} />
+      <img 
+      className="card-photo"
+      src={recipe.strMealThumb} 
+      alt={recipe.strMeal} 
+      />
       <div className="card-body">
-       <div className="card-name">{name}</div>
-       <div className="card-category">{category}</div>
+       <div className="card-name">{recipe.strMeal}</div>
+       <div className="card-category">{recipe.strCategory}</div>
        <div className="card-footer">
          <button className="btn-save">Save Recipe</button>
        </div>
@@ -16,6 +21,13 @@ function RecipeCard({name, category, image}) {
 }
 
 function App(){
+  const [recipes, setRecipes]=useState([]);
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+    .then((r) => r.json())
+    .then((data)=> setRecipes(data.meals));
+
+  })
   return (
     <div>
       <nav>
@@ -28,37 +40,22 @@ function App(){
         </div>
         <div className="saved-pill">
           <span>Saved</span>
-          <span className="saved-pill-count"></span>
+          <span className="saved-pill-count">0</span>
         </div>
       </nav>
       
        <div ClassName="section-wrap">
-        <div ClassName="recipe-grid">
-          <RecipeCard
-          name ="Teriyaki Chicken"
-          category= "Chicken"
-          image="https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_16:9/k%2FPhoto%2FRecipes%2F2024-05-chicken-teriyaki-190%2Fchicken-teriyaki-190-171-horizontal" />
-            <RecipeCard
-          name ="Chicken briyani"
-          category= "Chicken"
-          image="https://www.cubesnjuliennes.com/wp-content/uploads/2020/01/Chicken-Biryani.jpg" />
-            <RecipeCard
-          name ="Chicken pizza"
-          category= "Chicken"
-          image="https://www.chefadora.com/_next/image?url=https%3A%2F%2Fchefadora-production.s3.ap-southeast-2.amazonaws.com%2Fmedium_1000529681_7684043161.jpg&w=640&q=75" />
-            <RecipeCard
-          name ="Chicken Tikka"
-          category= "Chicken"
-          image="https://flavorquotient.com/wp-content/uploads/2020/04/Chicken-Tikka-FQ-4-1-of-1.jpg" />
-            <RecipeCard
-          name ="Chicken wrap  "
-          category= "Chicken"
-          image="https://sailorbailey.com/wp-content/uploads/2023/04/Cajun-Chicken-Wrap.jpg" />
-            <RecipeCard
-          name ="Chicken Burger"
-          category= "Chicken"
-          image="https://www.willcookforsmiles.com/wp-content/uploads/2013/07/Buffalo-Chicken-Burgers-close-crop.jpg" />
-       </div>
+        <div className="section-heading">
+          <span className="section-title">
+          Results
+          </span>
+          <span className="section-count">{recipes.length}Recipes</span>
+        </div>
+        <div className="recipe-grid">
+          {recipes.map((recipe)=>(
+            <RecipeCard key={recipe.idMeal} recipe={recipe} />
+          ))}
+        </div>
       </div>
     </div>
   );
